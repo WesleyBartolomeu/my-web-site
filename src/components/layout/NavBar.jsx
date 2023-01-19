f09cddf19608f8fa4f0a4f28ff37beb0c3754cc7
Wesley Bartolomeu <88113002+WesleyBartolomeu@users.noreply.github.com>
2023-01-19 14:26:58 -0300

    NavBar 2.0 Lateral esquerda

--- src/components/layout/NavBar.jsx
+++ src/components/layout/NavBar.jsx
@@ -4,8 +4,6 @@ import { Link } from 'react-router-dom'
 import mainLogo from "../images/Logo_wbs_big.png"
 import mainLogoW from "../images/logo_w_small.png"
 import weshome from '../images/foto_wes_home.jpeg'
-import { DiSass, DiHtml5, DiReact, DiGit} from "react-icons/di";
-import { IoLogoJavascript,IoLogoWordpress } from "react-icons/io";
 import Home from '../pages/Home'
 
 
@@ -13,23 +11,15 @@ import Home from '../pages/Home'
 
 function NavBar() {
 
-    return(
-            <>
+return(         
             <nav className={styles.navbar}>
-                    <Link to="/"><img src={mainLogoW} alt="" /></Link>
-                        <ul>            
-                            <li><Link to={"/"}>Home</Link></li>
-                            <li><Link to={"/projetos"}>Projetos</Link></li>
-                            <li><Link to={"/contato"}>Contato</Link></li>
-                        </ul>
-            </nav>
-
-
-        
-           
-            </>
-            
-    )
+                    <ul>            
+                        <li><Link to={"/"}>Home</Link></li>
+                        <li><Link to={"/projetos"}>Projetos</Link></li>
+                        <li><Link to={"/contato"}>Contato</Link></li>
+                    </ul>
+            </nav>                      
+ )
 }   
 
 export default NavBar
