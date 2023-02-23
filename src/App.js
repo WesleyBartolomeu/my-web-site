627adfed1b2aa1251935812799f9c518f2728a62
Wesley Bartolomeu <88113002+WesleyBartolomeu@users.noreply.github.com>
2023-02-22 21:06:05 -0300

    Remoçao da importaçao do router que impedia compila

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 
 import './App.css';
-import { BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import Footer from './components/layout/Footer';
 import Home from './components/pages/Home';
 import Container from './components/layout/Container';
